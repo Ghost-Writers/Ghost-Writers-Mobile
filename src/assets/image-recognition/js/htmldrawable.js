7e4a45e0484468cfd8d3744e06000825d01c6d23
Sho Nguyen <shonozaki@Shos-MacBook-Pro.local>
2017-07-12 15:21:32 -0700

    working on ajax requests

--- src/assets/image-recognition/js/htmldrawable.js
+++ src/assets/image-recognition/js/htmldrawable.js
@@ -101,4 +101,70 @@ var World = {
 
 World.init();
 
+var headers = {
+	'Content-Type': 'application/json',
+	'X-Token': '',
+	'X-Version': ''
+}
+
+var allMarkers = [];
+var newMarker;
+
+var TC_ID = '5963b498f67e6315b7658a2a';
+var target_Id = null;
+var baseUrl = 'https://api/wikitude.com';
+var path_add_target  = '/cloudrecognition/targetcollection' + TC_ID + 'target';
+var path_add_tagets  = '/cloudrecognition/targetcollection' + TC_ID + 'targets';
+var path_get_target  = '/cloudrecognition/targetcollection' + TC_ID + 'targets' + target_Id;
+var path_generate_TC = '/cloudrecognition/targetcollection' + TC_ID + '/generation/cloudarchive';
+
+/////// AJAX request to wikitude API ////////////////
+let getAllTargets = function () {
+	let path = baseUrl + path_add_target
+	$.ajax({
+		type: 'GET',
+		url: path,
+		headers: headers,
+		success: function(markers) {
+		  allMarkers = markers;
+		  alert('success getting all markers');
+		},
+		error: function(err) {
+			alert('failure getting all markers');
+		}
+	})
+}
+
+let addTarget = function(targetName, photoUrl) {
+	let target = {
+		name: targetName,
+		imageUrl: photoUrl
+	}
+	let path = baseUrl + path_add_target;
+	$.ajax({
+		type: 'POST',
+		url: path,
+		data: target,
+		headers: headers,
+		success: function(marker) {
+		  newMarker = marker;
+		  target_Id = marker.id;
+		  alert('success making new marker');
+		},
+		error: function(err) {
+			alert('failure making new marker');
+		}
+	})
+}
+
+let deleteTarget = function ()
+$.ajax({
+		type: 'GET',
+		url: path,
+		data: '',
+		headers: {},
+		success: '',
+		error: ''
+	})
+
 
