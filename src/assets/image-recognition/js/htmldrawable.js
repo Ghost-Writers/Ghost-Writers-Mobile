ed91431f68e1197b192246a99a8f25130709b732
Sho Nguyen <shonozaki@Shos-MacBook-Pro.local>
2017-07-13 11:51:29 -0700

    made ajax requests to wikitude api

--- src/assets/image-recognition/js/htmldrawable.js
+++ src/assets/image-recognition/js/htmldrawable.js
@@ -83,6 +83,21 @@ var World = {
 
 	},
 
+	openPicker: function openPicker() {
+		alert('in art')
+      var client = filestack.init("AQqITGUBVQ6mumB5gvo95z");
+        client.pick({});
+	},
+
+	captureScreen: function captureScreenFn() {
+		getAllTargets();
+		// AR.logger.info("captureScreen called ...");
+
+		// if (World.initialized) {
+		// 	document.location = "architectsdk://button?action=captureScreen";
+		// }
+	},
+
 	worldLoaded: function worldLoadedFn() {
 		var cssDivInstructions = " style='display: table-cell;vertical-align: middle; text-align: right; width: 50%; padding-right: 15px;'";
 		var cssDivSurfer = " style='display: table-cell;vertical-align: middle; text-align: left; padding-right: 15px; width: 38px'";
@@ -101,10 +116,15 @@ var World = {
 
 World.init();
 
+//////// filestack picker ///////////////////////////
+
+
+
+/////// AJAX request to wikitude API ////////////////
 var headers = {
 	'Content-Type': 'application/json',
-	'X-Token': '',
-	'X-Version': ''
+	'X-Token': 'fe087579ece1518a3a67dae3021888c7',
+	'X-Version': 3
 }
 
 var allMarkers = [];
@@ -112,25 +132,26 @@ var newMarker;
 
 var TC_ID = '5963b498f67e6315b7658a2a';
 var target_Id = null;
-var baseUrl = 'https://api/wikitude.com';
-var path_add_target  = '/cloudrecognition/targetcollection' + TC_ID + 'target';
-var path_add_tagets  = '/cloudrecognition/targetcollection' + TC_ID + 'targets';
-var path_get_target  = '/cloudrecognition/targetcollection' + TC_ID + 'targets' + target_Id;
-var path_generate_TC = '/cloudrecognition/targetcollection' + TC_ID + '/generation/cloudarchive';
+var baseUrl = 'https://api.wikitude.com';
+var path_add_target  = '/cloudRecognition/targetCollection/' + TC_ID + '/target/';
+var path_get_target  = '/cloudRecognition/targetCollection/' + TC_ID + '/target/' + target_Id;
+var path_generate_TC = '/cloudRecognition/targetCollection/' + TC_ID + '/generation/cloudarchive';
 
-/////// AJAX request to wikitude API ////////////////
 let getAllTargets = function () {
 	let path = baseUrl + path_add_target
+	alert('path is ' + path)
 	$.ajax({
 		type: 'GET',
 		url: path,
 		headers: headers,
 		success: function(markers) {
 		  allMarkers = markers;
-		  alert('success getting all markers');
+		  alert('success getting all markers' + allMarkers);
 		},
 		error: function(err) {
-			alert('failure getting all markers');
+			document.getElementById('loadingMessage').innerHTML =
+			// "<div> error is " + err + "</div>"
+			alert(JSON.stringify(err));
 		}
 	})
 }
@@ -157,14 +178,34 @@ let addTarget = function(targetName, photoUrl) {
 	})
 }
 
-let deleteTarget = function ()
-$.ajax({
-		type: 'GET',
+let deleteTarget = function (){
+	let path = baseUrl + path_get_target;
+	$.ajax({
+		type: 'DELETE',
 		url: path,
-		data: '',
-		headers: {},
-		success: '',
-		error: ''
+		headers: headers,
+		success: function() {
+		  alert('success deleting marker');
+		},
+		error: function(err) {
+			alert('failure deleting marker');
+		}
 	})
+}
 
+let generateTargetCollection = function () {
+	let path = baseUrl + path_generate_TC;
+	$.ajax({
+		type: 'POST',
+		url: path,
+		headers: headers,
+		success: function(collection) {
+		  newMarker = marker;
+		  alert('success generating collection');
+		},
+		error: function(err) {
+			alert('failure generating collection');
+		}
+	})
+}
 
